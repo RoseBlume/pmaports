739f088774860f13d479f9dc76f43f67ce51abff
Luca Weiss <luca@z3ntu.xyz>
2018-09-14 18:11:02 +0200

    Add dts and install dtb, disable default dhcpd to get 'normal' IP

--- device/device-pine-a64lts/uboot-script.cmd
+++ device/device-pine-a64lts/uboot-script.cmd
@@ -1,9 +1,9 @@
-setenv bootargs init=/init.sh rw console=tty0 console=ttyS0,115200 no_console_suspend earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0 loglevel=1
+setenv bootargs init=/init.sh rw console=tty0 console=ttyS0,115200 no_console_suspend earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0 loglevel=1 PMOS_NO_OUTPUT_REDIRECT
 
 printenv
 
 echo Loading DTB
-load mmc ${mmc_bootdev}:1 ${fdt_addr_r} dtb-pine-a64lts.dtb
+load mmc ${mmc_bootdev}:1 ${fdt_addr_r} sun50iw1p1-pine64so.dtb
 
 echo Loading Initramfs
 load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} uInitrd-pine-a64lts
