fa355c78cec81b2d8b5896f65ab3a7d9da37217e
Luca Weiss <luca@z3ntu.xyz>
2018-09-25 00:16:55 +0200

    testing

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 apk add git
 
 echo "----------"
@@ -8,4 +10,13 @@ echo "----------"
 git config --list
 echo "----------"
 git status
+echo "----------"
+git remote -v
+echo "----------"
+git remote add upstream https://gitlab.com/postmarketOS/pmaports
+echo "----------"
+git fetch upstream
+echo "----------"
+git rev-parse upstream/master
+git rev-parse HEAD
 echo "----------"
