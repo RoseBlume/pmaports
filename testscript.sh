530a05446ba6c83c991e86429a3d1af2a48cf075
Luca Weiss <luca@z3ntu.xyz>
2018-09-25 01:24:57 +0200

    remove old commands

--- testscript.sh
+++ testscript.sh
@@ -5,18 +5,7 @@ set -x
 apk add git
 
 echo "----------"
-pwd
-echo "----------"
-git config --list
-echo "----------"
-git status
-echo "----------"
-git remote -v
-echo "----------"
 git remote add upstream https://gitlab.com/postmarketOS/pmaports
 echo "----------"
 git fetch upstream
-echo "----------"
-git rev-parse upstream/master
-git rev-parse HEAD
 echo "----------"
