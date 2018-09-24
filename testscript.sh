f6ec166ceeb9a754e1255788f59b4a36846d53c7
Luca Weiss <luca@z3ntu.xyz>
2018-09-25 01:25:51 +0200

    simulate me being upstream

--- testscript.sh
+++ testscript.sh
@@ -5,7 +5,7 @@ set -x
 apk add git
 
 echo "----------"
-git remote add upstream https://gitlab.com/postmarketOS/pmaports
+git remote add upstream https://gitlab.com/z3ntu/pmaports
 echo "----------"
 git fetch upstream
 echo "----------"
