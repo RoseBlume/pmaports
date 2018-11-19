86d10a67bc86d2411ba19341ca3a7cb7d29f2c18
Piotr Halama <skrzynka@halamix2.pl>
2018-11-19 16:14:35 +0100

    sony-taoshan: fix typos and names

--- device/device-sony-taoshan/initfs-hook.sh
+++ device/device-sony-taoshan/initfs-hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Start the screen enlightener script"
-/bin/sh /usr/sbin/screen_enlightener >/tmp/screen_enlightener.log 2>&1 </dev/null &
+/bin/sh /usr/sbin/sony-taoshan-screen-enlightener >/tmp/sony-taoshan-screen-enlightener.log 2>&1 </dev/null &
