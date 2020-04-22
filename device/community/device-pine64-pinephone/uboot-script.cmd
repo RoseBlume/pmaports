1df6f9d3d0ac2f6a3514ae8dc70684f1cd04aecf
Bart Ribbers <bribbers@disroot.org>
2020-04-22 21:36:05 +0200

    pine64-pinephone: move to community (MR 1175)
    [ci:skip-build]: already built successfully in CI

---
+++ device/community/device-pine64-pinephone/uboot-script.cmd
@@ -0,0 +1,34 @@
+gpio set 98
+gpio set 114
+
+if test ${mmc_bootdev} -eq 0 ; then
+	echo "Booting from SD";
+	setenv bootdev 0;
+else
+	echo "Booting from eMMC";
+	setenv bootdev 2;
+fi;
+
+setenv bootargs init=/init.sh rw console=tty0 console=ttyS0,115200 no_console_suspend earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0 loglevel=1 cma=256M PMOS_NO_OUTPUT_REDIRECT pmos_boot=/dev/mmcblk${bootdev}p1 pmos_root=/dev/mmcblk${bootdev}p2
+
+printenv
+
+echo Loading DTB
+load mmc ${mmc_bootdev}:1 ${fdt_addr_r} sun50i-a64-pinephone-1.1.dtb
+
+echo Loading Initramfs
+load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} uInitrd-postmarketos-allwinner
+
+echo Loading Kernel
+load mmc ${mmc_bootdev}:1 ${kernel_addr_r} vmlinuz-postmarketos-allwinner
+
+gpio set 115
+
+echo Resizing FDT
+fdt addr ${fdt_addr_r}
+fdt resize
+
+echo Booting kernel
+gpio set 116
+gpio clear 98
+booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
