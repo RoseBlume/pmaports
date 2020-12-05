cc7d1795b20ab654e248c895150ccf5bb3992a38
Daniele Debernardi <drebrez@gmail.com>
2020-12-05 01:36:47 +0100

    Force reboot on kernel panic

--- main/postmarketos-mkinitfs-hook-kernel-panic/00-kernel-panic.sh
+++ main/postmarketos-mkinitfs-hook-kernel-panic/00-kernel-panic.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-echo "PMOS DEBUG: kernel panic in 30s" > /dev/kmsg
-sleep 30s
+echo "PMOS DEBUG: kernel panic in 15s" > /dev/kmsg
+sleep 15s
+
+# Reboot immediately on panic
+echo -1 > /proc/sys/kernel/panic
+
+# Panic immediately when a hung task is detected
+echo 1 > /proc/sys/kernel/hung_task_panic
+
+# Print all tasks info on panic
+echo 0 > /proc/sys/kernel/panic_print
 
 echo "PMOS DEBUG: kernel panic now" > /dev/kmsg
 echo c > /proc/sysrq-trigger
