b78a0ca78f7d3cfa3b5813a8f2ef3df956444e23
Oliver Smith <ollieparanoid@postmarketos.org>
2021-11-26 01:25:21 +0100

    remove packages not supported in v21.12
    * device/testing, device/unmaintained
    * cross/*-armhf, cross/*-x86
    * gcc4 and gcc6
    * postmarketos-ui-asteroid

--- device/testing/device-acer-swing/initfs-hook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Fix brightness
-echo 91 > /sys/class/leds/lcd-backlight/brightness
-
--- device/testing/device-jolla-sbj/led-charging-sbj.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-logger "Battery is in charging state"
-echo 100 > /sys/class/leds/led:rgb_red/brightness
