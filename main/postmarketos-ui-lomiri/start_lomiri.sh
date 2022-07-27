4ce0b82af9712b1ce819625e179ab903c24c0a10
Luca Weiss <luca@z3ntu.xyz>
2022-07-27 19:43:11 +0200

    main/postmarketos-ui-lomiri: add tinydm launching

---
+++ main/postmarketos-ui-lomiri/start_lomiri.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/lib/qt5/plugins/platforms
+export MIR_SERVER_CURSOR=null
+export QT_QPA_PLATFORM=mirserver
+#export G_MESSAGES_DEBUG=all
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export MIR_MESA_KMS_DISABLE_MODESET_PROBE=1 # probably only needed in QEMU
+export MIR_SERVER_ENABLE_MIRCLIENT=1
+
+dbus-run-session lomiri
