c56d305101d2f51c92946b6d983833068e5a3292
Oliver Smith <ollieparanoid@postmarketos.org>
2022-11-03 21:27:48 +0100

    CI: move parse_apkbuild_checksums to common (MR 3615)
    Move parse_source_from_checksums() from .ci/testcases/test_source.py to
    .ci/lib/common.py and give it the more fitting name
    parse_apkbuild_checksums(). Create a symlink for common.py to make it
    also available in .ci/lib/common.py.
    
    This is in preparation for using it in
    .ci/lib/check_changed_aports_versions.py in a future patch.

--- .ci/lib/common.py
+++ .ci/lib/common.py
@@ -2,7 +2,8 @@
 # Copyright 2021 Oliver Smith
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Various functions used in CI scripts
+# Various functions used in CI scripts and tests. This file is in
+# .ci/lib/common.py and symlinked to .ci/testcases/common.py.
 
 import configparser
 import os
@@ -189,4 +190,52 @@ def get_changed_packages():
 
         ret.add(os.path.basename(dirname))
 
+    return ret
+
+
+def parse_apkbuild_checksums(args, apkbuild_path):
+    """
+    Read the APKBUILD file and parse source files from the checksums at the
+    bottom. This has always the same format, even if $source is built with hard
+    to parse shell code (like in postmarketos-base).
+
+    :param apkbuild_path: full path to the APKBUILD
+    :returns: dict of parsed "source" filenames and checksums, e.g.:
+              {"first.patch": "b4dc4f3…",
+               "second.patch": "b4dc4f3…"}
+    """
+    start = 'sha512sums="'
+    in_block = False
+    ret = {}
+
+    with open(apkbuild_path, encoding="utf-8") as handle:
+        for line in handle.readlines():
+            # Find start
+            if not in_block:
+                if line.startswith(start):
+                    in_block = True
+                else:
+                    continue
+
+            # sha512sums may have lines without checksums:
+            # https://gitlab.alpinelinux.org/alpine/abuild/-/merge_requests/73
+            if "  " not in line:
+                continue
+
+            try:
+                checksum, filename = line.rstrip().split("  ", 2)
+            except ValueError:
+                raise ValueError("Failed to parse checksums. Try to delete the"
+                                 " checksums and generate them again with"
+                                 f" 'pmbootstrap checksum': {apkbuild_path}")
+
+            # Cut off 'sha512sums="' if the first checksum is in that line
+            if checksum.startswith(start):
+                checksum = checksum[len(start):]
+
+            # Find end
+            if filename.endswith('"'):
+                filename = filename[:-1]
+
+            ret[filename] = checksum
     return ret
--- .ci/testcases/test_source.py
+++ .ci/testcases/test_source.py
@@ -9,59 +9,15 @@ import os
 import pytest
 import sys
 
+# Same dir
+import common
+
+# pmbootstrap
 import add_pmbootstrap_to_import_path
 import pmb.parse
 import pmb.parse._apkbuild
 import pmb.parse.apkindex
 import pmb.helpers.repo
-
-
-def parse_source_from_checksums(args, apkbuild_path):
-    """
-    Read the APKBUILD file and parse source files from the checksums at the
-    bottom. This has always the same format, even if $source is built with hard
-    to parse shell code (like in postmarketos-base).
-
-    :param apkbuild_path: full path to the APKBUILD
-    :returns: dict of parsed "source" filenames and checksums, e.g.:
-              {"first.patch": "b4dc4f3…",
-               "second.patch": "b4dc4f3…"}
-    """
-    start = 'sha512sums="'
-    in_block = False
-    ret = {}
-
-    with open(apkbuild_path, encoding="utf-8") as handle:
-        for line in handle.readlines():
-            # Find start
-            if not in_block:
-                if line.startswith(start):
-                    in_block = True
-                else:
-                    continue
-
-            # sha512sums may have lines without checksums:
-            # https://gitlab.alpinelinux.org/alpine/abuild/-/merge_requests/73
-            if "  " not in line:
-                continue
-
-            try:
-                checksum, filename = line.rstrip().split("  ", 2)
-            except ValueError:
-                raise ValueError("Failed to parse checksums. Try to delete the"
-                                 " checksums and generate them again with"
-                                 f" 'pmbootstrap checksum': {apkbuild_path}")
-
-            # Cut off 'sha512sums="' if the first checksum is in that line
-            if checksum.startswith(start):
-                checksum = checksum[len(start):]
-
-            # Find end
-            if filename.endswith('"'):
-                filename = filename[:-1]
-
-            ret[filename] = checksum
-    return ret
 
 
 def test_aports_unreferenced_files(args):
@@ -74,7 +30,7 @@ def test_aports_unreferenced_files(args):
             continue
 
         apkbuild = pmb.parse.apkbuild(apkbuild_path)
-        sources_chk = parse_source_from_checksums(args, apkbuild_path)
+        sources_chk = common.parse_apkbuild_checksums(args, apkbuild_path)
 
         # Collect files from subpackages
         subpackage_installs = []
@@ -116,7 +72,7 @@ def test_distfiles_conflict(args):
     """
     source_all = {}
     for apkbuild_path in glob.iglob(f"{args.aports}/**/APKBUILD", recursive=True):
-        source = parse_source_from_checksums(args, apkbuild_path)
+        source = common.parse_apkbuild_checksums(args, apkbuild_path)
         dir_path = os.path.dirname(apkbuild_path)
         apkbuild_rel = os.path.relpath(apkbuild_path, args.aports)
         for filename, checksum in source.items():
