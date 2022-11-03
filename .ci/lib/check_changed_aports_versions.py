c5fa233f373e80040cec7e888347416ed841003d
Oliver Smith <ollieparanoid@postmarketos.org>
2022-11-03 23:35:15 +0100

    CI: aports version check: modernize with new rules (MR 3615)
    Update the script once more with the goal that we no longer need to
    comment on pkgrel/pkgver bumps. Parse not only the version of the old
    and new package, but also the checksums and remote sources.
    
    Replace the previous check of simply having $pkgver-r$pkgrel higher
    than the upstream APKBUILD with more detailed rules:
    
      APKBUILD has remote sources?      ------>       Did sha512sums change?
       |                                  No            |         |
       | Yes                                            | No      | Yes
       v                                                |         v
      Did the file name of a remote                     |     * Increase pkgver
      source change?                    ----.           |     * Set pkgrel=0
       |                                    |           |
       | Yes                                | No        |
       v                                    v           v
      Is pkgver=9999?         ----->   * Don't change pkgver
      (Forked from Alpine)     Yes     * Bump pkgrel by 1
       |
       | No
       v
      * Set the pkgver to the version
        of the source tarball
      * Set pkgrel=0
    
    I'll also put this in the wiki at:
    https://postmarketos.org/howto-bump-pkgrel-pkgver
    
    Besides the new rules, print a detailed error message below each package
    where the test failed that explains exactly what was expected and what
    the reasoning behind that is. Also add a hint that users can run this
    locally with 'pmbootstrap ci' now.
    
    Use logging instead of print, so we get colored ERROR: and NOTE:
    statements from pmbootstrap code, this should make it easier to read.

--- .ci/lib/check_changed_aports_versions.py
+++ .ci/lib/check_changed_aports_versions.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-# Copyright 2021 Oliver Smith
+# Copyright 2022 Oliver Smith
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import glob
-import tempfile
-import sys
+import logging
+import os
 import subprocess
+import sys
+import tempfile
 
 # Same dir
 import common
@@ -17,7 +19,19 @@ import pmb.parse.version
 import pmb.helpers.logging
 
 
-def get_package_version(args, package, revision, check=True):
+def get_package_version_sources(args, package, revision, check=True):
+    """
+    Get the version ($pkgver-r$pkgrel), checksums and a list of remote files
+    (not bundled with the APKBUILD) from a specific git revision.
+    :param package: name of the package
+    :param revision: git revision to look at
+    :param check: complain if the file does not exist
+    :returns: {"checksums": see lib.common.parse_apkbuild_checksums(),
+               "pkgrel": 3,
+               "pkgver": "1.0.0",
+               "sources_remote": ["x-1.0.0-r2.tar.bz2", ...],
+               "version": "1.0.0-r2"}
+    """
     # Redirect stderr to /dev/null, so git doesn't complain about files not
     # existing in upstream branch for new packages
     stderr = None
@@ -36,11 +50,27 @@ def get_package_version(args, package, revision, check=True):
     # Save APKBUILD to a temporary path and parse it from there. (Not the best
     # way to do things, but good enough for this CI script.)
     with tempfile.TemporaryDirectory() as tempdir:
-        with open(tempdir + "/APKBUILD", "w", encoding="utf-8") as handle:
+        apkbuild_path = f"{tempdir}/APKBUILD"
+        with open(apkbuild_path, "w", encoding="utf-8") as handle:
             handle.write(apkbuild_content)
-        parsed = pmb.parse.apkbuild(tempdir + "/APKBUILD", False, False)
+        parsed = pmb.parse.apkbuild(apkbuild_path, False, False)
+        checksums = common.parse_apkbuild_checksums(args, apkbuild_path)
 
-    return parsed["pkgver"] + "-r" + parsed["pkgrel"]
+    # Build sources_remote from files in checksums that are not bundled.
+    # To make the logic not overly complicated, only glob in the currently
+    # checked out directory. This is inaccurate for the previous commit, but
+    # good enough for how it's used in check_versions() below.
+    sources_remote = []
+    for filename, checksum in checksums.items():
+        bundled_path_pattern = f"{os.path.dirname(path)}/**/{filename}"
+        if not glob.glob(bundled_path_pattern, recursive=True):
+            sources_remote += filename
+
+    return {"checksums": checksums,
+            "pkgrel": int(parsed['pkgrel']),
+            "pkgver": parsed['pkgver'],
+            "sources_remote": sources_remote,
+            "version": f"{parsed['pkgver']}-r{parsed['pkgrel']}"}
 
 
 def version_compare_operator(result):
@@ -57,29 +87,19 @@ def version_compare_operator(result):
 
 def exit_with_error_message():
     branch = common.get_upstream_branch()
-    print()
-    print("ERROR: Modified package(s) don't have an increased version or a")
-    print("new package has a nonzero pkgrel!")
-    print()
-    print("This can happen if you added a new package with a nonzero")
-    print("pkgrel, or if you did not change the pkgver/pkgrel")
-    print("variables in the APKBUILDs. Or you did change them, but the")
-    print(f"packages have been updated in the official '{branch}' branch, and")
-    print("now your versions are not higher anymore.")
-    print()
-    print("Your options:")
-    print("a) If you made changes to the packages, and did not increase the")
-    print("   pkgrel/pkgver: increase them now, and force push your branch.")
-    print("   => https://postmarketos.org/howto-bump-pkgrel-pkgver")
-    print("b) If you had already increased the package versions, rebase on")
-    print(f"   '{branch}', increase the versions again and then force push:")
-    print("   => https://postmarketos.org/rebase")
-    print("c) If you made a change, that does not require rebuilding the")
-    print("   packages, such as only changing the arch=... line: you can")
-    print("   disable this check by adding '[ci:skip-vercheck]' to the")
-    print("   latest commit message, then force push.")
-    print()
-    print("Thank you and sorry for the inconvenience.")
+    logging.error("")
+    logging.error("NOTE: use 'pmbootstrap ci commits' to run this check"
+                  " on your PC.")
+    logging.error("")
+    logging.error("Some more hints:")
+    logging.error(f"* You may need to rebase on {branch}.")
+    logging.error("* Detailed rules for bumping pkgrel/pkgver:")
+    logging.error("  https://postmarketos.org/howto-bump-pkgrel-pkgver")
+    logging.error("* If the error doesn't make sense (e.g. you want to")
+    logging.error("  intentionally downgrade a package), add [ci:skip-build]")
+    logging.error("  with reasoning to the last commit message.")
+    logging.error("")
+    logging.error("Thank you and sorry for the inconvenience.")
     exit(1)
 
 
@@ -96,33 +116,80 @@ def check_versions(args, packages):
     commit = f"upstream/{common.get_upstream_branch()}"
     if common.run_git(["rev-parse", "HEAD"]) == common.run_git(["rev-parse",
                                                                 commit]):
-        print(f"NOTE: {commit} is on same commit as HEAD, comparing"
-              " HEAD against HEAD~1.")
+        logging.info(f"NOTE: {commit} is on same commit as HEAD, comparing"
+                     " HEAD against HEAD~1.")
         commit = "HEAD~1"
 
     for package in packages:
-        # Get versions, skip new packages
-        head = get_package_version(args, package, "HEAD")
-        upstream = get_package_version(args, package, commit, False)
+        logging.info("")
+        # Get versions and sources, skip new packages
+        head = get_package_version_sources(args, package, "HEAD")
+        head_v = head["version"]
+        upstream = get_package_version_sources(args, package, commit, False)
         if not upstream:
-            if head.rpartition('r')[2] != "0":
-                print(f"- {package}: {head} (HEAD) (new package) [ERROR]")
+            if head["pkgrel"] != 0:
+                logging.error(f"- {package}: {head_v} (HEAD) (new package)")
+                logging.error("  ERROR: new package should have pkgrel=0")
                 error = True
             else:
-                print(f"- {package}: {head} (HEAD) (new package)")
+                logging.info(f"- {package}: {head_v} (HEAD) (new package)")
             continue
+        upstream_v = upstream["version"]
 
-        # Compare head and upstream versions
-        result = pmb.parse.version.compare(head, upstream)
+        # Compare head and upstream versions based on rules from:
+        # https://postmarketos.org/howto-bump-pkgrel-pkgver
+        error_str = ""
+        if head["sources_remote"]:
+            if head["sources_remote"] == upstream["sources_remote"]:
+                pkgver = upstream["pkgver"]
+                pkgrel = upstream["pkgrel"] + 1
+                if head["pkgver"] != pkgver or head["pkgrel"] != pkgrel:
+                    error_str = f"expected pkgver={pkgver}, pkgrel={pkgrel}" \
+                                " (pkgver unchanged, pkgrel + 1), because" \
+                                " this package has unchanged remote sources"
+            else:  # remote sources changed
+                if upstream["pkgver"] == "9999":
+                    pkgver = "9999"
+                    pkgrel = upstream["pkgrel"] + 1
+                    if head["pkgver"] != pkgver or head["pkgrel"] != pkgrel:
+                        error_str = f"expected pkgver={pkgver}," \
+                                    f" pkgrel={pkgrel} (pkgver unchanged," \
+                                    " pkgrel + 1), because this package has" \
+                                    " pkgver=9999"
+                elif head["pkgver"] == upstream["pkgver"] or head["pkgrel"] != 0:
+                    error_str = "expected pkgver to change and pkgrel=0," \
+                                " because this package has changed remote" \
+                                " sources"
+        else:  # no remote sources
+            if head["checksums"] == upstream["checksums"]:
+                pkgver = upstream["pkgver"]
+                pkgrel = upstream["pkgrel"] + 1
+                if head["pkgver"] != pkgver or head["pkgrel"] != pkgrel:
+                    error_str = f"expected pkgver={pkgver}, pkgrel={pkgrel} " \
+                                " (pkgver unchanged, pkgrel + 1), because" \
+                                " this package has no remote sources and the" \
+                                " bundled sources didn't change"
+            else:  # checksums changed
+                if head["pkgver"] == upstream["pkgver"] or head["pkgrel"] != 0:
+                    error_str = "expected pkgver to change and pkgrel=0," \
+                                " because this package has no remote sources" \
+                                " but the bundled files changed"
+
+        result = pmb.parse.version.compare(head_v, upstream_v)
         if result != 1:
-            error = True
+            error_str = "new version is not higher than the previous version"
 
         # Print result line ("- hello-world: 1-r2 (HEAD) > 1-r1 (HEAD~1)")
         formatstr = "- {}: {} (HEAD) {} {} ({})"
-        if result != 1:
-            formatstr += " [ERROR]"
         operator = version_compare_operator(result)
-        print(formatstr.format(package, head, operator, upstream, commit))
+        logging.error(formatstr.format(package, head_v, operator, upstream_v,
+                                       commit))
+
+        # Detailed error message should help the user to resolve it and explain
+        # why it failed.
+        if error_str:
+            logging.error(f"  ERROR: {error_str}")
+            error = True
 
     if error:
         exit_with_error_message()
@@ -132,18 +199,18 @@ if __name__ == "__main__":
     # Get and print modified packages
     common.add_upstream_git_remote()
     packages = common.get_changed_packages()
-    print(f"Changed packages: {packages}")
+    logging.info(f"Changed packages: {packages}")
 
     # Verify modified package count
     common.get_changed_packages_sanity_check(len(packages))
     if len(packages) == 0:
-        print("no aports changed in this branch")
+        logging.info("no aports changed in this branch")
         exit(0)
 
     # Potentially skip this check
     if common.commit_message_has_string("[ci:skip-vercheck]"):
-        print("WARNING: not checking for changed package versions"
-              " ([ci:skip-vercheck])!")
+        logging.warning("WARNING: not checking for changed package versions"
+                        " ([ci:skip-vercheck])!")
         exit(0)
 
     # Initialize args (so we can use pmbootstrap's APKBUILD parsing)
@@ -152,5 +219,5 @@ if __name__ == "__main__":
     pmb.helpers.logging.init(args)
 
     # Verify package versions
-    print("checking changed package versions...")
+    logging.info("Checking changed package versions...")
     check_versions(args, packages)
