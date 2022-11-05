c2797d430162b4eb515c830f66344f0586591ab0
Luca Weiss <luca@z3ntu.xyz>
2022-11-05 08:53:13 +0100

    main/postmarketos-ui-lomiri: clean up launch script

--- main/postmarketos-ui-lomiri/start_lomiri.sh
+++ main/postmarketos-ui-lomiri/start_lomiri.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/usr/lib/qt5/plugins/platforms
 export MIR_SERVER_CURSOR=null
-export QT_QPA_PLATFORM=mirserver
-#export G_MESSAGES_DEBUG=all
+export QT_QPA_PLATFORM=wayland
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-export MIR_MESA_KMS_DISABLE_MODESET_PROBE=1 # probably only needed in QEMU
 export MIR_SERVER_ENABLE_MIRCLIENT=1
+
+export QT_IM_MODULE=maliit
+export MALIIT_FORCE_DBUS_CONNECTION=1
+export UITK_ICON_THEME=suru # ?
+
+#export MIR_MESA_KMS_DISABLE_MODESET_PROBE=1 # probably only needed in QEMU
+#export MIR_MESA_KMS_USE_DRM_DEVICE=card1 # for PinePhone
+#export QT_SCALE_FACTOR=2 # pinephone
+
+#export G_MESSAGES_DEBUG=all
 
 lomiri
