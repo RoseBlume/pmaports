dandelion64-Archives/pmaports-mt6762

main/pmos-ui-phosh: use superd for user services
This uses gnome-session to run superd by changing the directory that
gnome-session searches for apps to autostart to one that has only a
.desktop file that runs superd. superd then starts/manages non-critical
services.

Some services in /etc/xdg/autostart are *critical* to
phosh/gnome-session, they are listed in a phosh session file.
gnome-session will continue to use /etc/xdg/autostart when searching for
these services, and gnome-session is still used to start those. They are
"critical" because stopping any of them causes the session to die. As
such, I do not believe it makes sense to manage those in superd anyways,
since if any die, the whole session (including superd) is doing down
with it.

main/postmarketos-ui-phosh/90-gnome-session-autostart.sh
@@ -1,0 +1,7 @@
+#!/bin/sh
+# this instructs gnome-session to load user-specified autostart apps from a
+# different directory than the default (/etc/xdg/autostart). Apps that are
+# required by gnome-session (e.g. things like gsd-sound, etc) are still started
+# even when this is set. By pointing it here, it'll start superd, which can
+# then start/supervise everything not requried by gnome-session.
+export GNOME_SESSION_AUTOSTART_DIR="/usr/lib/superd/"

RoseBlume/pmaports

main/postmarketos-ui-phosh/90-gnome-session-autostart.sh
@@ -1,0 +1,7 @@
+#!/bin/sh
+# this instructs gnome-session to load user-specified autostart apps from a
+# different directory than the default (/etc/xdg/autostart). Apps that are
+# required by gnome-session (e.g. things like gsd-sound, etc) are still started
+# even when this is set. By pointing it here, it'll start superd, which can
+# then start/supervise everything not requried by gnome-session.
+export GNOME_SESSION_AUTOSTART_DIR="/usr/share/superd/"