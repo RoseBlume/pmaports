b78a5f77dcae9b60dd32119ba5fb7706e7d3b25c
Luca Weiss <luca@z3ntu.xyz>
2023-02-13 23:58:42 +0100

    main/postmarketos-ui-lomiri: enable device-specific tweaks when necessary

--- main/postmarketos-ui-lomiri/start_lomiri.sh
+++ main/postmarketos-ui-lomiri/start_lomiri.sh
@@ -9,9 +9,15 @@ export QT_IM_MODULE=maliit
 export MALIIT_FORCE_DBUS_CONNECTION=1
 export UITK_ICON_THEME=suru # ?
 
-#export MIR_MESA_KMS_DISABLE_MODESET_PROBE=1 # probably only needed in QEMU
-#export MIR_MESA_KMS_USE_DRM_DEVICE=card1 # for PinePhone
 #export QT_SCALE_FACTOR=2 # pinephone
+
+# Device-specific adjustments
+device_codename="$(grep deviceinfo_codename /etc/deviceinfo | cut -d\" -f2)"
+if [ "$device_codename" = "qemu-amd64" ]; then
+	export MIR_MESA_KMS_DISABLE_MODESET_PROBE=1
+elif [ "$device_codename" = "pine64-pinephone" ]; then
+	export MIR_MESA_KMS_USE_DRM_DEVICE=card1
+fi
 
 #export G_MESSAGES_DEBUG=all
 
