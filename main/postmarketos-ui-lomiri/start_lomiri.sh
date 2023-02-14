97f681263cde440b809e54cfc9b3cbee374a6928
Luca Weiss <luca@z3ntu.xyz>
2023-02-14 19:25:04 +0100

    main/postmarketos-ui-lomiri: fix up launching maliit-server from dbus

--- main/postmarketos-ui-lomiri/start_lomiri.sh
+++ main/postmarketos-ui-lomiri/start_lomiri.sh
@@ -10,6 +10,9 @@ export MALIIT_FORCE_DBUS_CONNECTION=1
 export UITK_ICON_THEME=suru # ?
 
 #export QT_SCALE_FACTOR=2 # pinephone
+#export G_MESSAGES_DEBUG=all
+
+dbus-update-activation-environment MALIIT_FORCE_DBUS_CONNECTION=1
 
 # Device-specific adjustments
 device_codename="$(grep deviceinfo_codename /etc/deviceinfo | cut -d\" -f2)"
@@ -18,8 +21,6 @@ if [ "$device_codename" = "qemu-amd64" ]; then
 elif [ "$device_codename" = "pine64-pinephone" ]; then
 	export MIR_MESA_KMS_USE_DRM_DEVICE=card1
 fi
-
-#export G_MESSAGES_DEBUG=all
 
 superd &
 
