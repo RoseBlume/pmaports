b983236b80f81b9ea445a432f2855de70cd0d1f3
Anton Bambura <jenneron@protonmail.com>
2024-01-02 13:36:01 +0200

    main/devicepkg-dev: add validation function for config fragments (MR 4678)
    There are some cases when kconfig fragments may be wrong:
    - a config option has been removed from kernel
    - a config depends on another config that we need to enable in the fragment
    - something else?
    
    This patch makes kernel_prepare.sh validate the resulting .config and
    end up with an error in case something is wrong. In these cases we
    have to adjust fragments to fix the problems.
    
    Closes #2490

--- main/devicepkg-dev/kernel_prepare.sh
+++ main/devicepkg-dev/kernel_prepare.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 kernel_configs=""
 
@@ -29,6 +29,40 @@ if [ -n "$fragments_from_package" ]; then
 	done
 fi
 
+# $1: config fragment name
+validate_config() {
+	_fragment="$1"
+	# builddir is defined at APKBUILD
+	# shellcheck disable=SC2154
+	_fragment_file="$builddir"/kernel/configs/"$_fragment"
+	# shellcheck disable=SC2154
+	_config_file="$builddir"/.config
+
+	# validate =y and =m values
+	while IFS= read -r line; do
+		if ! grep -q "$line" "$_config_file"; then
+			echo "ERROR: $line must be set! Please fix $_fragment fragment!"
+			EXIT_CODE=1
+		fi
+	done < <(grep -Eo "^CONFIG.*=[ym]" "$_fragment_file")
+
+	# validate =n values
+	while IFS= read -r line; do
+		if ! grep -q "# ${line%=n} is not set" "$_config_file"; then
+			echo "ERROR: ${line%=n} must NOT be set! Please fix $_fragment fragment!"
+			EXIT_CODE=1
+		fi
+	done < <(grep -Eo "^CONFIG.*=n" "$_fragment_file")
+
+	# validate ="something" values
+	while IFS= read -r line; do
+		if ! grep -q "$line" "$_config_file"; then
+			echo "ERROR: ${line%=*} must be set to ${line#*=}! Please fix $_fragment fragment!"
+			EXIT_CODE=1
+		fi
+	done < <(grep -Eo "^CONFIG.*=\".*\"" "$_fragment_file")
+}
+
 # mainline is default config fragment
 add_config "mainline"
 
@@ -41,8 +75,7 @@ fi
 # copy fragments
 # shellcheck disable=SC2154
 # builddir is defined in APKBUILD
-for config in $kernel_configs;
-do
+for config in $kernel_configs; do
 	_paths="/usr/share/devicepkg-dev/config-fragments/$config $srcdir/$config"
 	for file in $_paths; do
 		[ -f "$file" ] && cp "$file" "$builddir"/kernel/configs
@@ -53,3 +86,21 @@ done
 # shellcheck disable=SC2086,SC2154
 # _carch and _pmos_defconfig are defined in APKBUILD
 make ARCH="$_carch" $_pmos_defconfig $kernel_configs
+
+# validate all configs. this will go through all of them, and will
+# print all errors. However, it will end the script only after going
+# through all configs, because we want to see all problems that
+# happen during applying the configs
+#
+# EXIT_CODE will be set to 1 in validate_config function if there is
+# at least one misconfiguration, so we will be able to end up with an
+# error on condition of this variable.
+EXIT_CODE=0
+for config in $kernel_configs;
+do
+	validate_config "$config"
+done
+
+if [ "$EXIT_CODE" -ne 0 ]; then
+	exit "$EXIT_CODE"
+fi
